11aad6e9a207db913c7dced920ac5c54d4303b4c
Jared Scott <jtscott@gmail.com>
2016-10-10 16:37:43 +0000

    0.30.1, scripted withings sensors, config breakdown and cleanup

---
+++ withings/withings_co2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+USERNAME=$(cat /home/pi/.homeassistant/withings/secrets.txt | cut -d , -f1)
+PASSWORD=$(cat /home/pi/.homeassistant/withings/secrets.txt | cut -d , -f2)
+CURUSER=$(cat /home/pi/.homeassistant/withings/secrets.txt | cut -d , -f3)
+DEVICEID=$(cat /home/pi/.homeassistant/withings/secrets.txt | cut -d , -f4)
+
+#Authenticate
+curl -s -c /home/pi/.homeassistant/withings/cookies.txt -b /home/pi/.homeassistant/withings/cookies.txt 'https://account.withings.com/connectionuser/account_login?appname=my2&appliver=a9467957&r=https%3A%2F%2Fhealthmate.withings.com%2F' --data 'email='"$USERNAME"'&password='"$PASSWORD"'&is_admin='
+#Grab Session
+SESSIONKEY=$(cat /home/pi/.homeassistant/withings/cookies.txt | grep session | cut -f7 -d$'\t')
+#Grab CO2
+curl -s -b /home/pi/.homeassistant/withings/cookies.txt "https://healthmate.withings.com/index/service/v2/measure" -H "Cookie: session_key="$SESSIONKEY"; current_user="$CURUSER"" --data "sessionid="$SESSIONKEY"&meastype=12"%"2C35&deviceid="$DEVICEID"&appname=my2&appliver=a9467957&apppfm=web&action=getmeashf" | jq .body.series[1].data[-1].value
