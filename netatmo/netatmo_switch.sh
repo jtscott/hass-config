f6a037d9005f45b64f0e899487805a46f4b14d13
Jared Scott <jtscott@gmail.com>
2016-10-23 20:42:51 +0000

    proper vpn url fix

--- netatmo/netatmo_switch.sh
+++ netatmo/netatmo_switch.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-USERNAME=$(cat /home/pi/.homeassistant/netatmo/secrets.txt | cut -d , -f3)
-PASSWORD=$(cat /home/pi/.homeassistant/netatmo/secrets.txt | cut -d , -f4)
-ENDPOINT=$(cat /home/pi/.homeassistant/netatmo/secrets.txt | cut -d , -f5)
+USERNAME=$(cat /home/pi/.homeassistant/netatmo/secrets.txt | cut -d , -f1)
+PASSWORD=$(cat /home/pi/.homeassistant/netatmo/secrets.txt | cut -d , -f2)
 
-curl -s 'https://4.vpn.netatmo.net/'"$ENDPOINT"'/'"$USERNAME"'/'"$PASSWORD"',/command/changestatus?status='"$1"''  > /dev/null
+#Start session
+curl -s -c /home/pi/.homeassistant/netatmo/cookies.txt -b /home/pi/.homeassistant/netatmo/cookies.txt https://auth.netatmo.com/en-US/access/login -H 'User-Agent: curl 7.38.0 (arm-unknown-linux-gnueabihf) libcurl/7.38.0 OpenSSL/1.0.1t zlib/1.2.8 libidn/1.29 libssh2/1.4.3 librtmp/2.3' > /dev/null
+#Grab CSRF
+CSRF=$(cat /home/pi/.homeassistant/netatmo/cookies.txt | grep csrf | cut -f7 -d$'\t')
+#Authenticate
+curl -s -c /home/pi/.homeassistant/netatmo/cookies.txt -b /home/pi/.homeassistant/netatmo/cookies.txt https://auth.netatmo.com/en-US/access/login -H 'User-Agent: curl 7.38.0 (arm-unknown-linux-gnueabihf) libcurl/7.38.0 OpenSSL/1.0.1t zlib/1.2.8 libidn/1.29 libssh2/1.4.3 librtmp/2.3' --data 'ci_csrf_netatmo='"$CSRF"'&mail='"$USERNAME"'&pass='"$PASSWORD"'&log_submit=LOGIN&stay_logged=accept' > /dev/null
+#Grab Status
+ACCESSTOKEN=$(cat /home/pi/.homeassistant/netatmo/cookies.txt | grep netatmocomaccess_token | cut -f7 -d$'\t' | sed 's/%7C/|/g')
+#jq grabs the vpn url from json, sed strips the quotes
+VPNURL=$(curl -s -b /home/pi/.homeassistant/netatmo/cookies.txt https://my.netatmo.com/api/gethomedata -H 'Authorization: Bearer '"$ACCESSTOKEN"'' -H 'Origin: https://my.netatmo.com' -H 'Content-Type: application/json;charset=UTF-8' -H 'Accept: application/json, text/plain, */*' -H 'User-Agent: curl 7.38.0 (arm-unknown-linux-gnueabihf) libcurl/7.38.0 OpenSSL/1.0.1t zlib/1.2.8 libidn/1.29 libssh2/1.4.3 librtmp/2.3' -H 'Referer: https://my.netatmo.com/app/camera' --data-binary '{}' | jq .body.homes[0].cameras[0].vpn_url | sed -e 's/^"//' -e 's/"$//')
+#Send the switch
+curl -s "$VPNURL/command/changestatus?status=$1"  > /dev/null
