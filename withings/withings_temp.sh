eb01c188bdf5831cd4e043356ab357606793c5af
Jared Scott <jtscott@gmail.com>
2017-02-11 20:02:11 -0700

    frickin typo

--- withings/withings_temp.sh
+++ withings/withings_temp.sh
@@ -6,7 +6,7 @@ CURUSER=$(cat /home/pi/.homeassistant/withings/secrets.txt | cut -d , -f3)
 DEVICEID=$(cat /home/pi/.homeassistant/withings/secrets.txt | cut -d , -f4)
 
 #Grab CSRFTOKEN
-curl -s 'https://account.withings.com/connectionuser/account_login?appname=my2&appliver=df77ca8f&r=https%3A%2F%2Fhealthmate.withings.com%2F' > crsf.txt
+curl -s 'https://account.withings.com/connectionuser/account_login?appname=my2&appliver=df77ca8f&r=https%3A%2F%2Fhealthmate.withings.com%2F' > csrf.txt
 CSRFTOKEN=$( grep csrf csrf.txt | awk -F'"' '$0=$6')
 #Authenticate
 curl -s -c /home/pi/.homeassistant/withings/cookies.txt -b /home/pi/.homeassistant/withings/cookies.txt 'https://account.withings.com/connectionuser/account_login?appname=my2&appliver=df77ca8f&r=https%3A%2F%2Fhealthmate.withings.com%2F' -H 'Origin: https://account.withings.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cache-Control: max-age=0' -H 'Referer: https://account.withings.com/connectionuser/account_login?appname=my2&appliver=df77ca8f&r=https%3A%2F%2Fhealthmate.withings.com%2F' -H 'Connection: keep-alive' -H 'DNT: 1' --data 'email='"$USERNAME"'&password='"$PASSWORD"'&is_admin=&csrf_token='"$CSRFTOKEN"'' --compressed > /dev/null
