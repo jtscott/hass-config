879b8dd921753ee705dda139a6db64cc0d58c2d1
Jared Scott <jtscott@gmail.com>
2017-03-02 18:26:30 -0700

    order of operations

--- prereq.sh
+++ prereq.sh
@@ -5,6 +5,7 @@ popd > /dev/null
 
 apt-get update && apt-get upgrade -y
 apt-get install -y flex bison gcc make autotools-dev npm
+npm install -g belkin-wemo-command-line-tools
 wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-1.5.tar.gz
 tar xfvz jq-1.5.tar.gz
 cd jq-1.5
@@ -12,8 +13,5 @@ cd jq-1.5
 make
 make install
 ln -s /usr/local/bin/jq /usr/bin/jq
-npm install -g belkin-wemo-command-line-tools
 rm -rf $SCRIPTPATH/jq-1.5/
 rm -rf $SCRIPTPATH/jq-1.5.tar.gz
-apt-get -y --purge autoremove
-apt-get clean
