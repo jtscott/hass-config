68df2e8c271b2c2c3855c76f2ccc4f9c3f35fdd3
Jared Scott <jtscott@gmail.com>
2017-05-19 23:31:46 -0600

    back to pywemo, better jq install

--- prereq.sh
+++ prereq.sh
@@ -3,15 +3,7 @@ pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
 popd > /dev/null
 
-apt-get update && apt-get upgrade -y
-apt-get install -y flex bison gcc make autotools-dev npm fontconfig 
-npm install -g belkin-wemo-command-line-tools
-wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-1.5.tar.gz
-tar xfvz jq-1.5.tar.gz
-cd jq-1.5
-./configure
-make
-make install
-ln -s /usr/local/bin/jq /usr/bin/jq
-rm -rf $SCRIPTPATH/jq-1.5/
-rm -rf $SCRIPTPATH/jq-1.5.tar.gz
+apt-get update && apt-get install jq
+wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+chmod +x jq-linux64
+mv jq-linux64 $(which jq)
