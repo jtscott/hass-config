bdcc486e63d0ef94d04d3536a62b34aea8fa3c58
Jared Scott <jtscott@gmail.com>
2017-09-10 10:34:01 -0600

    fixed for nokia changes

--- withings/withings_co2.sh
+++ withings/withings_co2.sh
@@ -7,10 +7,12 @@ USERNAME=$(cat $SCRIPTPATH/secrets.txt | cut -d , -f1)
 PASSWORD=$(cat $SCRIPTPATH/secrets.txt | cut -d , -f2)
 CURUSER=$(cat $SCRIPTPATH/secrets.txt | cut -d , -f3)
 DEVICEID=$(cat $SCRIPTPATH/secrets.txt | cut -d , -f4)
+STARTDATE=$(date +%s -d "28 day ago")
+ENDDATE=$(date +%s)
 
 #Authenticate
-curl -s -c $SCRIPTPATH/cookies.txt -b $SCRIPTPATH/cookies.txt 'https://account.withings.com/connectionuser/account_login?appname=my2&appliver=e5ed95dd&r=https%3A%2F%2Fhealthmate.withings.com%2F' -H 'Origin: https://account.withings.com' -H 'Referer: https://account.withings.com/connectionuser/account_login?appname=my2&appliver=e5ed95dd&r=https%3A%2F%2Fhealthmate.withings.com%2F' -H 'Connection: keep-alive' -H 'DNT: 1' --data 'email='"$USERNAME"'&password='"$PASSWORD"'&is_admin=' --compressed > /dev/null
+curl -s -c $SCRIPTPATH/cookies.txt -b $SCRIPTPATH/cookies.txt 'https://account.health.nokia.com/connectionwou/account_login?r=https%3A%2F%2Fdashboard.health.nokia.com%2F' -H 'Pragma: no-cache' -H 'Origin: https://account.health.nokia.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'Cache-Control: no-cache' -H 'Referer: https://account.health.nokia.com/connectionwou/account_login?r=https%3A%2F%2Fdashboard.health.nokia.com%2F' -H 'Connection: keep-alive' -H 'DNT: 1' --data 'email='"$USERNAME"'&password='"$PASSWORD"'&is_admin=' --compressed > /dev/null
 #Grab Session
-SESSIONKEY=$(cat $SCRIPTPATH/cookies.txt | grep session | cut -f7 -d$'\t')
+SESSIONKEY=$(cat $SCRIPTPATH/cookies.txt | grep nokia | grep session | cut -f7 -d$'\t')
 #Grab CO2
-curl -s -c $SCRIPTPATH/cookies.txt -b $SCRIPTPATH/cookies.txt "https://healthmate.withings.com/index/service/v2/measure" -H "Cookie: session_key="$SESSIONKEY"; current_user="$CURUSER"" -H 'Origin: https://healthmate.withings.com' --data "sessionid="$SESSIONKEY"&meastype=12"%"2C35&deviceid="$DEVICEID"&appname=my2&appliver=a9467957&apppfm=web&action=getmeashf" | jq .body.series[1].data[-1].value
+curl -s -c $SCRIPTPATH/cookies.txt -b $SCRIPTPATH/cookies.txt 'https://scalews.withings.net/cgi-bin/v2/measure' -H 'Pragma: no-cache' -H 'Origin: https://dashboard.health.nokia.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' -H 'Content-Type: text/plain;charset=UTF-8' -H 'Accept: */*' -H 'Cache-Control: no-cache' -H 'Referer: https://dashboard.health.nokia.com/'"$CURUSER"'/environment/'"$DEVICEID"'' -H 'Connection: keep-alive' -H 'DNT: 1' --data-binary 'meastype=35%2C12&deviceid='"$DEVICEID"'&userid='"$CURUSER"'&startdate='"$STARTDATE"'&enddate='"$ENDDATE"'&sessionid='"$SESSIONKEY"'&action=getmeashf&appname=hmw&apppfm=web&appliver=19fe0fa' --compressed | jq .body.series[0].data[-1].value
