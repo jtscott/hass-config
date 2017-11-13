73b0dad2ce65a721208d51c3342fab51aca5b4d2
Jared Scott <jtscott@gmail.com>
2017-11-13 16:21:20 -0700

    simplify jq install

--- prereq.sh
+++ prereq.sh
@@ -3,7 +3,8 @@ pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
 popd > /dev/null
 
-apt-get update && apt-get install jq
-wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
-chmod +x jq-linux64
-mv jq-linux64 $(which jq)
+if [ ! -f /usr/bin/jq ]; then
+  wget https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
+  chmod +x jq-linux64
+  mv jq-linux64 /usr/bin/jq
+fi
