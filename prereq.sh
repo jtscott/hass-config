3a868425fcb506643dd7965cd13bebad0a794774
Jared Scott <jtscott@gmail.com>
2018-03-14 15:38:13 -0600

    moving houses! get rid of old sensors, add some new

--- prereq.sh
+++ prereq.sh
@@ -8,3 +8,11 @@ if [ ! -f /usr/bin/jq ]; then
   chmod +x jq-linux64
   mv jq-linux64 /usr/bin/jq
 fi
+
+pip3 install --upgrade --force-reinstall netdisco
+apt-get update && apt-get --yes --force-yes upgrade
+apt-get install --yes --force-yes avahi-daemon avahi-utils
+update-rc.d dbus enable
+update-rc.d avahi-daemon enable
+/etc/init.d/dbus start
+/etc/init.d/avahi-daemon start
